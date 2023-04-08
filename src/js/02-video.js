f28418d2170cb38c23f717a9108238761c5ff104
AnetRo <beatrice.ccq@gmail.com>
2023-04-09 01:02:49 +0300

    02 m

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import LodashTh from 'lodash.throttle';
+
+const throttle = LodashTh('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on("timeupdate", onPlay);
+
+function onPlay({seconds}) {
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds)
+    
+    // console.log(seconds)
+    // console.log(typeof seconds)
+      
+} 
+console.log(localStorage.getItem(LOCALSTORAGE_KEY))
+const time = localStorage.getItem(LOCALSTORAGE_KEY)
+
+player.setCurrentTime(time)
+
+
+
+// .then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+
+
+
+
+
+ 
+
+
 
