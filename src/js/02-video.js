817d43d8b203df3c9cfcb35c676782a3b76dd9b8
AnetRo <beatrice.ccq@gmail.com>
2023-04-09 22:27:42 +0300

    02 & 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,11 @@
 import Player from '@vimeo/player';
-import LodashTh from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-const throttle = LodashTh('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-player.on("timeupdate", onPlay);
+player.on("timeupdate", throttle(onPlay, 1000));
 
 function onPlay({seconds}) {
     
@@ -22,20 +21,6 @@ const time = localStorage.getItem(LOCALSTORAGE_KEY)
 player.setCurrentTime(time)
 
 
-
-// .then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea")
+};
+
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {}
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", throttle(onTextareaInput, 1000));
+
+refs.form.addEventListener("input", event => {
+   
+    formData[event.target.name] = event.target.value;
+    
+});
+
+savedTextarea();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    // console.log("send form")
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onTextareaInput(event) {
+    const message = event.target.value;
+    localStorage.setItem(STORAGE_KEY, message)
+};
+
+function savedTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
